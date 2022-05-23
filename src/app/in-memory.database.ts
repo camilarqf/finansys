9ae5cda82d90e086b8ce75738343c9561c9be7d7
Camila Fonseca <camilarqf@gmail.com>
2022-05-22 21:16:10 -0300

    feature: adiciona in-memory-database para categorias

---
+++ src/app/in-memory.database.ts
@@ -0,0 +1,17 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+export class InMemoryDatabase implements InMemoryDbService{
+
+  createDb(){
+    const categorias = [
+      {id: 1, nome: 'Moradia', descricao: 'Pagamentos de conta da casa'},
+      {id: 2, nome: 'Saúde', descricao: 'Plano de saúde e remédios'},
+      {id: 3, nome: 'Lazer', descricao: 'Cinema, parque, praias e etc...'},
+      {id: 4, nome: 'Salário', descricao: 'Recebimento de salário'},
+      {id: 5, nome: 'Freelas', descricao: 'Trabalhos como freelancer'},
+    ];
+
+    return categorias;
+  }
+
+}
